f624ac1108aa42a28cd028ff2efa2c11eff929fd
seisvalt <jl.vargass@gmail.com>
2022-04-19 21:36:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+    <h1 align="center">Gridview Extension for Yii 2</h1>
+    <br>
+</p>
+
+This extension of Gridview for [Yii framework 2.0](http://www.yiiframework.com).
+
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+
+
+to the require section of your composer.json.
+
+> Note: Version requires PHP .
+
+Usage
+-----
+
---
+++ src/GridView.php
@@ -0,0 +1,101 @@
+<?php
+namespace seisvalt\widgets;
+
+use yii\base\Widget;
+use kartik\export\ExportMenu;
+use kartik\grid\GridView;
+use yii\helpers\Html;
+
+class GridView extends Widget
+{
+    public $dataProvider;
+    public $gridColumns;
+    public $searchModel;
+    public $pdfConfig;
+
+    public function init()
+    {
+        parent::init();
+
+        if(!$this->pdfConfig) {
+            $this->pdfConfig =  [
+                'methods' => [
+                    'SetTitle' => 'Sin titulo',
+                    'SetSubject' => 'Subject',
+                    'SetHeader' => ['Archivo generado el: ' . date("r")],
+                    'SetFooter' => ['|Page {PAGENO}|'],
+                    'SetAuthor' => 'Seisvalt',
+                    'SetCreator' => 'Seisvalt',
+                    'SetKeywords' => 'Yii2, Export, PDF, MPDF, Output, GridView, Grid, yii2-grid, yii2-mpdf, yii2-export',
+                ]
+            ];
+        }
+    }
+
+    public function run()
+    {
+        $customDropdown = [
+            'options' => ['tag' => false],
+            'linkOptions' => ['class' => 'dropdown-item']
+        ];
+
+        $fullExportMenu = ExportMenu::widget([
+            'dataProvider' => $this->dataProvider,
+            'columns' => $this->gridColumns,
+            'target' => ExportMenu::TARGET_BLANK,
+            'asDropdown' => false,
+            'pjaxContainerId' => 'kv-pjax-container',
+            'exportConfig' => [
+                ExportMenu::FORMAT_TEXT => false,
+                ExportMenu::FORMAT_HTML => false,
+                ExportMenu::FORMAT_EXCEL => false,
+                ExportMenu::FORMAT_CSV => $customDropdown,
+                ExportMenu::FORMAT_EXCEL_X => $customDropdown,
+                ExportMenu::FORMAT_PDF => [
+                    'options' => ['tag' => false],
+                    'linkOptions' => ['class' => 'dropdown-item'],
+                    'pdfConfig' => $this->pdfConfig
+                ],
+            ],
+            'exportContainer' => [
+            ],
+            'clearBuffers' => true,
+            'dropdownOptions' => [
+                'label' => '<i class="fas fa-external-link-alt"></i>Exportar Todo',
+            ],
+        ]);
+
+        GridView::widget([
+            'dataProvider' => $this->dataProvider,
+            'filterModel' => $this->searchModel,
+            'columns' => $this->gridColumns,
+            'toolbar' => [
+                [
+                    'options' => ['class' => 'btn-group-sm mr-2'],
+                ],
+                '{export}',
+            ],
+            'exportContainer' => [
+                'class' => 'btn-group-sm mr-2 me-2'
+            ],
+            'exportConfig' => [
+                'csv' => [],
+                'xls' => [],
+                'pdf' => [],
+            ],
+            'export' => [
+                'itemsAfter'=> [
+                    '<div role="presentation" class="dropdown-divider"></div>',
+                    '<div class="dropdown-header">Exportar Todo</div>',
+                    $fullExportMenu
+                ]
+            ],
+            'responsive' => true,
+            'panel' => [
+                'type' => 'blue',
+                'heading' => '<i class="fas fa-book"></i>  ' .  Yii::t('app', 'List'),
+                'after' => false,
+            ],
+        ]);
+    }
+}
